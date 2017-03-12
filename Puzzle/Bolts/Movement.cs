49a436eca419ba389f009254411c0c9ca05d6c63
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-03-13 02:50:18 +0300

    Fixed bugs with boards.

--- Puzzle/Bolts/Movement.cs
+++ Puzzle/Bolts/Movement.cs
@@ -7,21 +7,22 @@ namespace Bolts
     public class Movement : MonoBehaviour
     {
         public Transform rightBoard;
+        public Transform leftBoard;
+        public Transform staticRightBoard;
+        public Transform staticLeftBoard;
         public Transform[] items;
 
         private Vector3 screenSpace;
         private Vector3 offset;
-        private readonly float staticRightBoard = 2.121f;
-        private readonly float staticLeftBoard = -3.321f;
-        private float rightBoardPosition;
-        private float leftBoardPosition;
+        public float boardOffset;
         private readonly float boardStep = 0.63f;
         private bool isGrab = false;
 
+        public Vector3 currentPosition;
+
         private void Start()
         {
-            rightBoardPosition = transform.position.x + staticRightBoard;
-            leftBoardPosition = transform.position.x + staticLeftBoard;
+            boardOffset = rightBoard.localPosition.x - transform.localPosition.x;
         }
 
         private void Update()
@@ -42,8 +43,8 @@ namespace Bolts
                     {
                         if (hit.collider.gameObject == gameObject)
                         {
-                            screenSpace = Camera.main.WorldToScreenPoint(transform.position);
-                            offset = transform.position -
+                            screenSpace = Camera.main.WorldToScreenPoint(transform.localPosition);
+                            offset = transform.localPosition -
                                 Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z));
                             isGrab = true;
                         }
@@ -61,23 +62,18 @@ namespace Bolts
             if (isGrab)
             {
                 var currentScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);
-                var currentPosition = Camera.main.ScreenToWorldPoint(currentScreenSpace) + offset;
+                currentPosition = Camera.main.ScreenToWorldPoint(currentScreenSpace) + offset;
 
-                var boardValue = GetBoard();
-                var boardOffset = rightBoard.position.x - transform.position.x;
+                var rightBoardValue = GetBoard();
 
-                Mathf.Clamp(currentPosition.x, );
-
-                if (currentPosition.x + boardOffset <= boardValue)
-                    transform.position = new Vector3(currentPosition.x, transform.position.y, transform.position.z);
-                else
-                    transform.position = new Vector3(boardValue - boardOffset, transform.position.y, transform.position.z);
+                currentPosition.x = Mathf.Clamp(currentPosition.x, staticLeftBoard.localPosition.x + boardOffset, rightBoardValue - boardOffset);
+                transform.localPosition = new Vector3(currentPosition.x, transform.localPosition.y, transform.localPosition.z);
             }
         }
 
         private float GetBoard()
         {
-            var board = rightBoardPosition;
+            var board = staticRightBoard.position.x;
             for(int i=0;i<items.Length;i++)
             {
                 if (items[i].GetComponent<Rotation>().rotated)
