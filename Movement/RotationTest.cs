8faa91112c4e145f49078e4fda9a95af0a643347
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-03-26 18:18:05 +0300

    PairProgramming: Ping-Pong (HeadBobbing Test) Dmitriy

--- Movement/RotationTest.cs
+++ Movement/RotationTest.cs
@@ -3,36 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RotationTest : MonoBehaviour {
-    public float minimuxY = -60.0f;
-    public float maximumY = 60.0f;
+	public float minimuxY = -60.0f;
+	public float maximumY = 60.0f;
 
-    public float sensitivity = 15.0f;
+	public float sensitivity = 15.0f;
 
-    private float rotationY = 0.0f;
+	private float rotationY = 0.0f;
 
-    private void OnEnable()
-    {
-        Cursor.visible = false;
-    }
+	private void OnEnable()
+	{
+		Cursor.visible = false;
+	}
 
-    private void OnDisable()
-    {
-        Cursor.visible = true;
-    }
+	private void OnDisable()
+	{
+		Cursor.visible = true;
+	}
 
-    private void Update()
-    {
-        Rotate();
-    }
+	private void Update()
+	{
+		Rotate();
+	}
 
-    private void Rotate()
-    {
-        float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
+	private void Rotate()
+	{
+		float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
 
-        rotationY += Input.GetAxis("Mouse Y") * sensitivity;
-        rotationY = Mathf.Clamp(rotationY, minimuxY, maximumY);
+		rotationY += Input.GetAxis("Mouse Y") * sensitivity;
+		rotationY = Mathf.Clamp(rotationY, minimuxY, maximumY);
 
-        Camera.main.transform.localEulerAngles = new Vector3(-rotationY, 0.0f, 0.0f);
-        transform.localEulerAngles = new Vector3(0.0f, rotationX, 0.0f);
-    }
+		Camera.main.transform.localEulerAngles = new Vector3(-rotationY, 0.0f, 0.0f);
+		transform.localEulerAngles = new Vector3(0.0f, rotationX, 0.0f);
+	}
 }
