bf1ddbeeaca529a865e28617f4778eddf09a6dce
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-03-27 00:21:11 +0300

    Pair programming: (Ping-pong) realization Footsteps

--- Movement/HeadBobbing.cs
+++ Movement/HeadBobbing.cs
@@ -2,53 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeadBobbing : MonoBehaviour
-{
-    private float timer = 0.0f;
-    public float bobbingSpeed = 0.18f;
-    public float bobbingAmount = 0.2f;
-    public float midpoint = 2.0f;
-    public Vector3 curLocalPosition;
+public class HeadBobbing : MonoBehaviour {
+	private float timer = 0.0f;
+	public float bobbingSpeed = 0.18f;
+	public float bobbingAmount = 0.2f;
+	public float midpoint = 2.0f;
+	public Vector3 curLocalPosition;
 
-    void Update()
-    {
-        float waveslice = 0.0f;
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+	void Update () {
+		float waveslice = 0.0f;
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
 
-        SetLocalPos(transform.localPosition);
-       
-        if (Mathf.Abs(horizontal) == 0 && Mathf.Abs(vertical) == 0)
-        {
-            timer = 0.0f;
-        }
-        else
-        {
-            waveslice = Mathf.Sin(timer);
-            timer += bobbingSpeed;
-            if (timer > Mathf.PI * 2)
-            {
-                timer = timer - (Mathf.PI * 2);
-            }
-        }
+		SetLocalPos (transform.localPosition);
 
-        if (waveslice != 0)
-        {
-            float translateChange = waveslice * bobbingAmount;
-            float totalAxes = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
-            totalAxes = Mathf.Clamp(totalAxes, 0.0f, 1.0f);
-            translateChange = totalAxes * translateChange;
-            curLocalPosition.y = midpoint + translateChange;
-        }
-        else
-        {
-            curLocalPosition.y = midpoint;
-        }
-        transform.localPosition = curLocalPosition;
-    }
-    public void SetLocalPos(Vector3 localPos)
-    {
-        this.curLocalPosition = localPos;
-        Debug.Log(localPos);
-    }
+		if (Mathf.Abs(horizontal) == 0 && Mathf.Abs(vertical) == 0) {
+			timer = 0.0f;
+		}
+		else {
+			waveslice = Mathf.Sin(timer);
+			timer += bobbingSpeed;
+			if (timer > Mathf.PI * 2) {
+				timer = timer - (Mathf.PI * 2);
+			}
+		}
+
+		if (waveslice != 0) {
+			float translateChange = waveslice * bobbingAmount;
+			float totalAxes = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+			totalAxes = Mathf.Clamp (totalAxes, 0.0f, 1.0f);
+			translateChange = totalAxes * translateChange;
+			curLocalPosition.y = midpoint + translateChange;
+		}
+		else {
+			curLocalPosition.y = midpoint;
+		}
+
+		transform.localPosition = this.curLocalPosition;
+	}
+
+	public void SetLocalPos(Vector3 position)
+	{
+		this.curLocalPosition = position;
+	}
 }
