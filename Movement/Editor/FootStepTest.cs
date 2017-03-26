c0ddb76237dbd03b06f157ce38dfd0d1b5bf91ab
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-03-27 00:23:40 +0300

    Pair programming: (Ping-pong) added test for FootStep

---
+++ Movement/Editor/FootStepTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class FootStepTest {
+
+	[Test]
+	public void EditorTest() {
+        FootSteps footStep = new FootSteps();
+        HeadBobbing headBob = new HeadBobbing();
+        headBob.curLocalPosition.y = 1.9f;
+
+        footStep.PlayFootStep(2);
+		Assert.AreEqual(1, footStep.clipNum);
+	}
+}
