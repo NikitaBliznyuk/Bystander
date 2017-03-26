24f948a76da25b33f8e123aefb151a8be846757e
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-03-27 00:24:46 +0300

    Pair programming (equally) MainMenu scene and Launch Scene script

---
+++ Menus/PlayButtonScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayButtonScript : MonoBehaviour {
+
+	public void RunScene()
+	{
+		SceneManager.LoadScene ("Beach test");
+	}
+}
