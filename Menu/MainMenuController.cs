cb54bf971f7df9909907e1314d59b724b29310d2
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-04-30 00:58:05 +0300

    Added main menu controller. Now player can start new game and exit from it.

---
+++ Menu/MainMenuController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene("Island");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
