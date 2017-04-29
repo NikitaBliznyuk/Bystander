99b1006bf0fdd5ac086ccf1449a6a5f02f8c74c2
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-04-29 20:30:11 +0300

    Added main menu.

---
+++ Menu/Preloader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Preloader : MonoBehaviour
+{
+    private float fadeTime = 2.0f;
+    private float currentTime = 0.0f;
+
+    private void Update()
+    {
+        if (currentTime < fadeTime)
+        {
+            currentTime += Time.deltaTime;
+        }
+        else
+        {
+            SceneManager.LoadScene("Main menu");
+        }
+    }
+}
